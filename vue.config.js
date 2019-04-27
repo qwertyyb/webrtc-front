33d862ffbfda063c98f9af283a324a0aeec637cb
qwertyyb <qwertyyb@qq.com>
2019-04-27 22:07:08 +0800

    include complied files

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,26 @@
 module.exports = {
+  publicPath: '',
+  assetsDir: 'assets',
   devServer: {
     proxy: {
       '^/socket.io': {
-        target: 'http://localhost:8443',
+        target: 'http://webrtc.qwertyyb.cn',
         changeOrigin: true,
         ws: true
       },
       '^/api': {
-        target: 'http://localhost:8443',
+        target: 'http://webrtc.qwertyyb.cn',
         changeOrigin: true
       }
+      // '^/socket.io': {
+      //   target: 'http://localhost:8443',
+      //   changeOrigin: true,
+      //   ws: true
+      // },
+      // '^/api': {
+      //   target: 'http://localhost:8443',
+      //   changeOrigin: true
+      // }
     }
   }
 }
